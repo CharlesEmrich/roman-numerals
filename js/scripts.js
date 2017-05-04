3ff9273e5bcaeb407d03115ea44098138e9915dc
CharlesEmrich <charles.emrich@gmail.com>
2017-05-04 13:07:18 -0700

    add js to pass specs 4 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 //Business Logic
-function parseOnes (num) {
+function parsePlacesAndConvert (num) {
   var output = "";
+
+  if (num.length === 1) {
+    output += parseOnes(num);
+  } else if (num.length === 2) {
+    output += parseTens(num[0]);
+    output += parseOnes(num[1]);
+  }
+  return output;
+}
+
+function parseOnes (numString) {
+  var output = "";
+  var num = parseInt(numString);
   if (num <= 3) {
     for (var i = 0; i < num; i++) {
       output += "I";
@@ -18,12 +31,30 @@ function parseOnes (num) {
   return output;
 }
 
+function parseTens (numString) {
+  var output = "";
+  var num = parseInt(numString);
+  if (num <= 3) {
+    for (var i = 0; i < num; i++) {
+      output += "X";
+    }
+  } else if (num === 4) {
+    output += "XL";
+  } else if (num > 4 && num < 9) {
+    output += "L";
+    for (var i = 0; i < num - 5; i++) {
+      output += "X";
+    }
+  } else if (num === 9) {
+    output += "XC";
+  }
+  return output;
+}
 //Front-End Logic
 $(function() {
   $("form").keyup(function() {
-    var userInput = parseInt($("input#user-input").val());
-    var output = parseOnes(userInput);
-
+    var userInput = $("input#user-input").val(); //Since I'm treating it like a string. Opt not to parseInt()?
+    var output = parsePlacesAndConvert(userInput);
     $("p#output").text(output);
   });
 });
